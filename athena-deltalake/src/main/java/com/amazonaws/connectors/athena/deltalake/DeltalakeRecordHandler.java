142630e4e6c39160c9c98c57f095b1db5b323d50
slomkarafa <slomkarafa@gmail.com>
2022-12-02 00:28:29 +0100

    athena-deltalake | close parquet readers connections and s3client back to default

--- athena-deltalake/src/main/java/com/amazonaws/connectors/athena/deltalake/DeltalakeRecordHandler.java
+++ athena-deltalake/src/main/java/com/amazonaws/connectors/athena/deltalake/DeltalakeRecordHandler.java
@@ -84,10 +84,7 @@ public class DeltalakeRecordHandler
 
     public DeltalakeRecordHandler(String dataBucket)
     {
-        this(AmazonS3ClientBuilder
-                .standard()
-                .withClientConfiguration(new ClientConfiguration().withMaxConnections(200))
-                .build(),
+        this(AmazonS3ClientBuilder.defaultClient(),
             AWSSecretsManagerClientBuilder.defaultClient(),
             AmazonAthenaClientBuilder.defaultClient(),
             new Configuration(),
@@ -150,10 +147,12 @@ public class DeltalakeRecordHandler
         String tablePath = String.format("s3a://%s/%s/%s", dataBucket, schemaName, tableName);
         String filePath = String.format("%s/%s", tablePath, relativeFilePath);
 
-        MessageType parquetSchema = ParquetFileReader.open(HadoopInputFile.fromPath(new Path(filePath), conf))
+        ParquetFileReader fileReader = ParquetFileReader.open(HadoopInputFile.fromPath(new Path(filePath), conf));
+        MessageType parquetSchema = fileReader
                 .getFooter()
                 .getFileMetaData()
                 .getSchema();
+        fileReader.close();
 
         List<Field> fields = recordsRequest.getSchema().getFields();
 
@@ -199,5 +198,6 @@ public class DeltalakeRecordHandler
             spiller.writeRows((block, rowNum) -> rowWriter.writeRow(block, rowNum, finalRecord) ? 1 : 0);
             record = reader.read();
         }
+        reader.close();
     }
 }
